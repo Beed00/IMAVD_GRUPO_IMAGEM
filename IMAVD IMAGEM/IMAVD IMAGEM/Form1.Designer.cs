0e240f5e4c02029012f9518d8f51ae26347e8ebe
JoaoAlvesISEP <1181926@isep.ipp.pt>
2023-03-22 21:33:24 +0000

    Load da imagem na picture box.

--- IMAVD IMAGEM/IMAVD IMAGEM/Form1.Designer.cs
+++ IMAVD IMAGEM/IMAVD IMAGEM/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.exitButton = new System.Windows.Forms.Button();
             this.loadImageButton = new System.Windows.Forms.Button();
+            this.pbox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox)).BeginInit();
             this.SuspendLayout();
             // 
             // exitButton
@@ -52,15 +54,25 @@
             this.loadImageButton.UseVisualStyleBackColor = true;
             this.loadImageButton.Click += new System.EventHandler(this.loadImageButton_Click);
             // 
+            // pbox
+            // 
+            this.pbox.Location = new System.Drawing.Point(41, 27);
+            this.pbox.Name = "pbox";
+            this.pbox.Size = new System.Drawing.Size(401, 295);
+            this.pbox.TabIndex = 2;
+            this.pbox.TabStop = false;
+            // 
             // appWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbox);
             this.Controls.Add(this.loadImageButton);
             this.Controls.Add(this.exitButton);
             this.Name = "appWindow";
             this.Text = "IMAVD IMAGEM";
+            ((System.ComponentModel.ISupportInitialize)(this.pbox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -69,6 +81,7 @@
 
         private System.Windows.Forms.Button exitButton;
         private System.Windows.Forms.Button loadImageButton;
+        private System.Windows.Forms.PictureBox pbox;
     }
 }
 
